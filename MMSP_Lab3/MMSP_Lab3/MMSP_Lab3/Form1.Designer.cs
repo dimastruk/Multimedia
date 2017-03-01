1d80a670cf190387440655925dd013169a8c89c8
Dmytro Struk <mr.struk@list.ru>
2017-03-01 15:01:04 +0200

    Lab #3.

---
+++ MMSP_Lab3/MMSP_Lab3/MMSP_Lab3/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace MMSP_Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Display = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ConstValue = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SinAngle = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.ConstValue)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SinAngle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Display
+            // 
+            this.Display.AccumBits = ((byte)(0));
+            this.Display.AutoCheckErrors = false;
+            this.Display.AutoFinish = false;
+            this.Display.AutoMakeCurrent = true;
+            this.Display.AutoSwapBuffers = true;
+            this.Display.BackColor = System.Drawing.Color.Black;
+            this.Display.ColorBits = ((byte)(32));
+            this.Display.DepthBits = ((byte)(16));
+            this.Display.Location = new System.Drawing.Point(-1, 0);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(480, 480);
+            this.Display.StencilBits = ((byte)(0));
+            this.Display.TabIndex = 0;
+            this.Display.Load += new System.EventHandler(this.Display_Load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(485, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Коефіцієнт \'a\':";
+            // 
+            // ConstValue
+            // 
+            this.ConstValue.Location = new System.Drawing.Point(487, 28);
+            this.ConstValue.Name = "ConstValue";
+            this.ConstValue.Size = new System.Drawing.Size(154, 20);
+            this.ConstValue.TabIndex = 5;
+            this.ConstValue.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.ConstValue.ValueChanged += new System.EventHandler(this.ConstValue_ValueChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.SinAngle);
+            this.groupBox1.Location = new System.Drawing.Point(487, 54);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(154, 51);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Кут повороту:";
+            // 
+            // SinAngle
+            // 
+            this.SinAngle.Location = new System.Drawing.Point(6, 19);
+            this.SinAngle.Maximum = new decimal(new int[] {
+            361,
+            0,
+            0,
+            0});
+            this.SinAngle.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.SinAngle.Name = "SinAngle";
+            this.SinAngle.Size = new System.Drawing.Size(142, 20);
+            this.SinAngle.TabIndex = 7;
+            this.SinAngle.ValueChanged += new System.EventHandler(this.SinAngle_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 478);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ConstValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Display);
+            this.Name = "Form1";
+            this.Text = "y = a*sin(x)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ConstValue)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.SinAngle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl Display;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown ConstValue;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown SinAngle;
+    }
+}
+
---
+++ MMSP_Lab3/MMSP_Lab3/MMSP_Lab3/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Windows.Forms;
+
+// для роботи з бібліотекою OpenGL 
+using Tao.OpenGl;
+// для роботи з бібліотекою FreeGLUT 
+using Tao.FreeGlut;
+
+namespace MMSP_Lab3
+{
+    public partial class Form1 : Form
+    {
+        const double Plane_Height = 20.0;
+        const double Plane_Width = 20.0;
+        int angle = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            Display.InitializeContexts();
+
+            ConstValue.Maximum = (decimal)Plane_Height/2;
+            ConstValue.Minimum = 1;
+        }
+
+        public void ShowDisplay(double plane_height, double plane_width)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_SINGLE);
+
+            Gl.glClearColor(255, 255, 255, 1);
+
+            Gl.glViewport(0, 0, Display.Width, Display.Height);
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+
+            if ((float)Display.Width <= (float)Display.Height)
+            {
+                Glu.gluOrtho2D(0.0, plane_height * (float)Display.Height / (float)Display.Width, 0.0, plane_width);
+            }
+            else
+            {
+                Glu.gluOrtho2D(0.0, plane_width * (float)Display.Width / (float)Display.Height, 0.0, plane_height);
+            }
+
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+        }
+
+        private void Display_Load(object sender, EventArgs e)
+        {
+            ShowDisplay(Plane_Height, Plane_Width);   
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            BuildCartesianPlane();
+        }
+
+        public void BuildCartesianPlane()
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glLoadIdentity();
+
+            Gl.glColor3d(0.8, 0.8, 0.8);
+
+            // Вертикальні лінії
+            Gl.glLineWidth(1);
+            for (int i = 0; i < Plane_Width; i++)
+                DrawLine(i, 0, i, Plane_Height);
+
+            // Горизонтальні лінії
+            Gl.glLineWidth(1);
+            for (int i = 0; i < Plane_Height; i++)
+                DrawLine(0, i, Plane_Width, i);
+
+            Gl.glColor3d(0.0, 0.0, 0.0);
+            Gl.glLineWidth(2);
+
+            // Вісь X
+            DrawLine(0, Plane_Height / 2, Plane_Width, Plane_Height / 2);
+            DrawLine(Plane_Width, Plane_Height / 2, Plane_Width - 0.5, (Plane_Height / 2) - 0.5);
+            DrawLine(Plane_Width, Plane_Height / 2, Plane_Width - 0.5, (Plane_Height / 2) + 0.5);
+
+            // Вісь Y
+            DrawLine(Plane_Width / 2, Plane_Height, Plane_Width / 2, 0);
+            DrawLine(Plane_Width / 2, Plane_Height, (Plane_Width / 2 - 0.5), Plane_Height - 0.5);
+            DrawLine(Plane_Width / 2, Plane_Height, (Plane_Width / 2 + 0.5), Plane_Height - 0.5);
+
+            // Підписи до осі X
+            for (int i = 0, k = -(int)(Plane_Width / 2); i < Plane_Width; i++, k++)
+                PrintText(i - 0.15, Plane_Height / 2 + 0.1, k.ToString());
+
+            // Підписи до осі Y
+            for (int i = 0, k = -(int)(Plane_Height / 2); i < Plane_Height; i++, k++)
+            {
+                if (k == 0) continue;
+                if (k < 0)
+                    PrintText(Plane_Width / 2, i - 0.15, k.ToString());
+                else
+                    PrintText(Plane_Width / 2 + 0.3, i - 0.15, k.ToString());
+            }
+
+            Gl.glFlush();
+            Display.Invalidate();
+        }
+
+        public void BuildGraphic()
+        {
+            BuildCartesianPlane();
+
+            Gl.glTranslated(Plane_Width / 2, Plane_Width / 2, 0);            
+            
+            Gl.glRotated(angle, 0.0, 0.0, 1.0);
+
+            for (double i = 0; i < Plane_Width; i += 0.001)
+                DrawPoint(i, (double)ConstValue.Value*Math.Sin(i));
+
+            for (double i = 0; i < Plane_Width; i += 0.001)
+                DrawPoint(-i, (double)ConstValue.Value*Math.Sin(-i));
+
+            Gl.glFlush();
+            Display.Invalidate();
+        }
+
+        public void DrawLine(double x1, double y1, double x2, double y2)
+        {
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+            Gl.glVertex2d(x1, y1);
+            Gl.glVertex2d(x2, y2);
+            Gl.glEnd();
+        }
+
+        public void DrawPoint(double x, double y)
+        {
+            Gl.glBegin(Gl.GL_POINTS);
+            Gl.glVertex2d(x, y);
+            Gl.glEnd();
+        }
+
+        public void PrintText(double x, double y, string text)
+        {
+            Gl.glRasterPos2f((float)x, (float)y);
+
+            foreach (var my_char in text)
+            { 
+                Glut.glutBitmapCharacter(Glut.GLUT_BITMAP_8_BY_13, my_char);
+            }
+        }
+
+        private void ConstValue_ValueChanged(object sender, EventArgs e)
+        {
+            BuildGraphic();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BuildGraphic();
+        }
+
+        private void SinAngle_ValueChanged(object sender, EventArgs e)
+        {
+            if (SinAngle.Value == 361) SinAngle.Value = 0;
+            if (SinAngle.Value == -1) SinAngle.Value = 360;
+            angle = (int)SinAngle.Value;
+            BuildGraphic();
+        }
+    }
+}
